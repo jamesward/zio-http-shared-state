0b6e4d88c831bf673b700431cba0c0daaa1573bd
James Ward <james@jamesward.com>
2022-12-02 10:47:10 -0700

    non-service ref

--- src/main/scala/MyServer.scala
+++ src/main/scala/MyServer.scala
@@ -11,20 +11,25 @@ import java.util.concurrent.TimeUnit
 
 object MyServer extends ZIOAppDefault:
 
-  val handler: ZIO[Ref.Synchronized[Int], Nothing, Response] =
+  def handler(ref: Ref.Synchronized[Int]): ZIO[Any, Nothing, Response] =
       for
-        ref <- ZIO.service[Ref.Synchronized[Int]]
         count <- ref.updateAndGet(_ + 1)
       yield
         Response.text(s"Count = $count")
 
-  val app = Http.collectZIO[Request] {
-    case Method.GET -> Path.root => handler
+  def app(ref: Ref.Synchronized[Int]) = Http.collectZIO[Request] {
+    case Method.GET -> Path.root => handler(ref)
   }
 
   val sharedStateLayer = ZLayer.scoped {
     Ref.Synchronized.make(0)
   }
 
+  val middleware = Middleware.collectZIO
+
   def run =
-    Server.serve(app).provide(Server.default ++ sharedStateLayer)
+    for
+      ref <- Ref.Synchronized.make(0)
+      server <- Server.serve(app(ref)).provide(Server.default)
+    yield
+      server
--- src/test/scala/MyServerSpec.scala
+++ src/test/scala/MyServerSpec.scala
@@ -1,3 +1,4 @@
+import zio.Ref
 import zio.http.*
 import zio.test.*
 import zio.test.Assertion.*
@@ -8,15 +9,17 @@ object MyServerSpec extends ZIOSpecDefault:
   def spec = suite("server")(
     test("should count one") {
       for
-        resp <- MyServer.app(Request.get(URL.root)).provideLayer(MyServer.sharedStateLayer)
+        ref <- Ref.Synchronized.make(0)
+        resp <- MyServer.app(ref)(Request.get(URL.root))
         body <- resp.body.asString
       yield
         assert(body)(equalTo("Count = 1"))
     },
     test("should count two sequential") {
       val check = for
-        _ <- MyServer.app(Request.get(URL.root))
-        resp <- MyServer.app(Request.get(URL.root))
+        ref <- Ref.Synchronized.make(0)
+        _ <- MyServer.app(ref)(Request.get(URL.root))
+        resp <- MyServer.app(ref)(Request.get(URL.root))
         body <- resp.body.asString
       yield
         assert(body)(equalTo("Count = 2"))
@@ -24,15 +27,14 @@ object MyServerSpec extends ZIOSpecDefault:
       check.provideLayer(MyServer.sharedStateLayer)
     },
     test("should count two parallel") {
-      val check = for
-        oneFork <- MyServer.app(Request.get(URL.root)).fork
-        twoFork <- MyServer.app(Request.get(URL.root)).fork
+      for
+        ref <- Ref.Synchronized.make(0)
+        oneFork <- MyServer.app(ref)(Request.get(URL.root)).fork
+        twoFork <- MyServer.app(ref)(Request.get(URL.root)).fork
         _ <- oneFork.join
         two <- twoFork.join
         body <- two.body.asString
       yield
         assert(body)(equalTo("Count = 2"))
-
-      check.provideLayer(MyServer.sharedStateLayer)
     },
   )
